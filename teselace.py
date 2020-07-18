f2b00f252b764e869132d3ee19a112404d7f2527
Jakub Komárek <komaja@email.cz>
2020-07-18 16:59:20 +0200

    Initial commit

---
+++ teselace.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+
+import tkinter as tk
+import tkinter.messagebox as mb
+import random
+import copy
+
+
+def random_color():
+    """
+    Generates a random string in format #XXXXXX
+    to be used as a color
+    """
+    code = random.randint(0, 0xFFFFFF)
+    return "#{:06X}".format(code)
+
+
+class Area(tk.Canvas):
+    INIT_TILES = 10
+    TILE_SIZE = 30
+
+    def __init__(self, master):
+        width = self.INIT_TILES * self.TILE_SIZE
+        height = self.INIT_TILES * self.TILE_SIZE
+
+        self.colors = {}
+
+        super().__init__(master, width=width, height=height, bg="white")
+
+    def update(self, data):
+        self.delete("all")
+
+        for y, row in enumerate(data):
+            for x, val in enumerate(row):
+                if val is None:
+                    continue
+
+                s = str(val)
+
+                if s in self.colors:
+                    color = self.colors[s]
+                else:
+                    color = random_color()
+                    self.colors[s] = color
+
+                left = x * self.TILE_SIZE
+                top = y * self.TILE_SIZE
+
+                self.create_rectangle(left, top, left + self.TILE_SIZE,
+                                      top + self.TILE_SIZE, fill=color)
+
+                x_center = left + self.TILE_SIZE / 2
+                y_center = top + self.TILE_SIZE / 2
+
+                self.create_text(x_center, y_center, text=s)
+
+    def set_size(self, width, height):
+        real_width = width * self.TILE_SIZE
+        real_height = height * self.TILE_SIZE
+
+        self.config(width=real_width, height=real_height)
+
+
+class App():
+    STEP_TILES = 4
+
+    def __init__(self, master):
+        self.area = Area(master)
+        self.area.pack(pady=100, padx=100)
+
+        self.width = 10
+        self.height = 10
+
+        frame = tk.Frame(master)
+        frame.pack()
+
+        self.width_entry = tk.Entry(frame)
+        self.width_entry.insert(0, "10")
+        self.width_entry.pack()
+
+        self.height_entry = tk.Entry(frame)
+        self.height_entry.insert(0, "10")
+        self.height_entry.pack()
+
+        size_button = tk.Button(frame, text="Update size",
+                                command=self.update_size)
+        size_button.pack(side=tk.LEFT)
+
+        step_button = tk.Button(frame, text="Step", command=self.step)
+        step_button.pack(side=tk.LEFT)
+
+        self.reset_state()
+
+    def update_size(self):
+        width = self.width_entry.get()
+        height = self.height_entry.get()
+
+        try:
+            width = int(width)
+            height = int(height)
+
+            if width < 1 or height < 1:
+                mb.showerror("Invalid sizes", "The sizes must be at least one")
+                return
+
+            self.area.set_size(width, height)
+
+            self.width = width
+            self.height = height
+
+            self.reset_state()
+
+        except ValueError:
+            mb.showerror("Invalid sizes", "The sizes are not integers")
+
+    def reset_state(self):
+        self.state = [[None for _ in range(self.width)]
+                      for _ in range(self.height)]
+        self.pos = (0, 0)
+        self.step_nu = 1
+
+        self.area.update(self.state)
+
+    def next_empty(self, pos):
+        if pos is None:
+            return None
+
+        x, y = pos
+
+        for j in range(y, self.height):
+            first = x if j == y else 0
+            for i in range(first, self.width):
+                if self.state[j][i] is None:
+                    return (i, j)
+
+        return None
+
+    def step(self):
+        pos = self.next_empty(self.pos)
+        self.pos = pos
+
+        valid = self.valid_step(pos)
+        if valid is None:
+            mb.showerror("No more steps", "There are no more valid steps")
+            return
+
+        for pos in valid:
+            x, y = pos
+            self.state[y][x] = self.step_nu
+
+        self.step_nu += 1
+
+        self.area.update(self.state)
+
+    def empty_neighbors(self, pos, state=None):
+        if state is None:
+            state = self.state
+
+        neighbors = [
+            (pos[0] - 1, pos[1]),
+            (pos[0] + 1, pos[1]),
+            (pos[0], pos[1] - 1),
+            (pos[0], pos[1] + 1)
+        ]
+
+        random.shuffle(neighbors)
+
+        for x, y in neighbors:
+            if x < 0 or y < 0 or x >= self.width or y >= self.height:
+                continue
+            if state[y][x] is not None:
+                continue
+            yield (x, y)
+
+    def valid_step(self, pos):
+        """
+        Returns a tuple of positions of a valid step
+        starting with pos
+        """
+        generators = []
+        curr_generated = [pos]
+
+        state_copy = copy.deepcopy(self.state)
+
+        if pos is None:
+            return None
+
+        x, y = pos
+        state_copy[y][x] = -1
+
+        new_gen = self.empty_neighbors(pos, state=state_copy)
+        generators.append(new_gen)
+
+        while generators:
+            last_gen = generators[-1]
+            try:
+                generated = next(last_gen)
+                curr_generated.append(generated)
+
+                x, y = generated
+                state_copy[y][x] = -1  # Placeholder
+
+                if len(curr_generated) == self.STEP_TILES:
+                    if self.is_finishable(state=state_copy):
+                        return tuple(curr_generated)
+                    state_copy[y][x] = None
+                    curr_generated.pop()
+                else:
+                    new_gen = self.empty_neighbors(generated, state=state_copy)
+                    generators.append(new_gen)
+
+            except StopIteration:
+                generators.pop()
+                x, y = curr_generated.pop()
+                state_copy[y][x] = None
+
+        return None
+
+    def is_finishable(self, state=None):
+        """
+        Do a DFS and check that all component sizes are divisible
+        by STEP_TILES
+        """
+
+        def dfs(x, y):
+            if x < 0 or y < 0 or x >= self.width or y >= self.height:
+                return 0
+            if state[y][x] is not None:
+                return 0
+            if visited[y][x]:
+                return 0
+
+            visited[y][x] = True
+            count = 1  # Me
+
+            neighbors = [
+                (x - 1, y),
+                (x + 1, y),
+                (x, y - 1),
+                (x, y + 1)
+            ]
+
+            for x2, y2 in neighbors:
+                count += dfs(x2, y2)
+
+            return count
+
+        if state is None:
+            state = self.state
+
+        visited = [[False for _ in range(self.width)]
+                   for _ in range(self.height)]
+
+        for y in range(self.width):
+            for x in range(self.height):
+                if visited[y][x] or state[y][x] is not None:
+                    continue
+                component_size = dfs(x, y)
+                if component_size % self.STEP_TILES != 0:
+                    return False
+
+        return True
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("2D Tesselation - Jakub Komárek 2020")
+    app = App(root)
+    root.mainloop()
---
+++ teselace_test.py
@@ -0,0 +1,110 @@
+"""
+Unit tests for 2D teselation program
+"""
+
+import unittest
+import tkinter as tk
+import itertools as it
+
+from parameterized import parameterized
+
+from teselace import App
+
+
+class TestApp(unittest.TestCase):
+    """
+    Tests for the App class
+    """
+    HEIGHT = 3
+    WIDTH = 3
+    STEP_TILES = 3
+
+    full = list(it.product(range(WIDTH), range(HEIGHT)))
+
+    def setUp(self):
+        root = tk.Tk()
+        self.app = App(root)
+
+        self.app.height = self.HEIGHT
+        self.app.width = self.WIDTH
+        self.app.STEP_TILES = self.STEP_TILES
+        self.app.reset_state()
+
+    def _fill_positions(self, full_positions):
+        for pos in full_positions:
+            x, y = pos
+            self.app.state[y][x] = 1
+
+    @parameterized.expand([
+        ("empty", [], (0, 0)),
+        ("first_full", [(0, 0)], (1, 0)),
+        ("row_full", [(0, 0), (1, 0), (2, 0)], (0, 1)),
+        ("last_left", full[:-1], (2, 2)),
+        ("full", full, None)
+    ])
+    def test_next_empty(self, _, full_positions, expected):
+        """
+        Parameterized test for next_empty method
+        """
+        self._fill_positions(full_positions)
+
+        next_empty = self.app.next_empty((0, 0))
+        self.assertEqual(next_empty, expected)
+
+    @parameterized.expand([
+        ("empty_corner", [], (0, 0), [(0, 1), (1, 0)]),
+        ("empty_side", [], (1, 0), [(0, 0), (1, 1), (2, 0)]),
+        ("empty_center", [], (1, 1), [(0, 1), (1, 0), (1, 2), (2, 1)]),
+        ("partly_blocked", [(0, 1), (1, 0)], (1, 1), [(1, 2), (2, 1)]),
+        ("blocked", [(0, 1), (1, 0), (1, 2), (2, 1)], (1, 1), [])
+    ])
+    def test_empty_neighbors(self, _, full_positions, pos, expected):
+        """
+        Parameterized test for empty_neighbors method
+        """
+        self._fill_positions(full_positions)
+
+        neighbors = sorted(list(self.app.empty_neighbors(pos)))
+        self.assertEqual(neighbors, expected)
+
+    @parameterized.expand([
+        ("empty", [], True),
+        ("full", full, True),
+        ("one_full", [(0, 0)], False),
+        ("one_good_component", [(0, 0), (1, 0), (2, 0)], True),
+        ("two_good_components", [(0, 1), (1, 1), (2, 1)], True),
+        ("one_bad_component", [(0, 0), (1, 0)], False),
+        ("two_bad_components", [(0, 0), (0, 1), (1, 1),
+                                (2, 0), (2, 1)], False),
+        ("one_good_one_bad", [(0, 0), (0, 1), (1, 1), (2, 0)], False)
+
+    ])
+    def test_is_finishable(self, _, full_positions, expected):
+        """
+        Parameterized test for is_finishable method
+        """
+        self._fill_positions(full_positions)
+
+        finishable = self.app.is_finishable()
+        self.assertEqual(finishable, expected)
+
+    def test_valid_step(self):
+        """
+        Parameterized test for valid_step method
+
+        For now we trust that the step is actually continuous
+        """
+        step = self.app.valid_step((0, 0))
+
+        self.assertIsNotNone(step)
+
+        for pos in step:
+            x, y = pos
+            self.app.state[y][x] = -1
+
+        finishable = self.app.is_finishable()
+        self.assertTrue(finishable)
+
+
+if __name__ == "__main__":
+    unittest.main()
