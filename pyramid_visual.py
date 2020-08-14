a78682ecddc5233129c83954b1f4e0ab54aa93d2
Jakub Komárek <komaja@email.cz>
2020-08-14 21:11:50 +0200

    Pyramid visualizer

--- pyramid_visual.py
+++ pyramid_visual.py
@@ -1,4 +1,4 @@
-from vpython import sphere, vec, rate, scene
+from vpython import sphere, arrow, vec, rate, scene, color
 from math import sqrt
 from time import sleep
 from random import random
@@ -8,7 +8,7 @@ RADIUS = 1
 
 spheres = []
 
-colors = dict()
+colors = {}
 
 
 def random_color():
@@ -44,13 +44,68 @@ def clear():
         del s
 
 
-model = PyramidCoveringModel(10, 4)
-model.reset()
+def show_axes():
+    OPACITY = 0.5
 
-try:
-    while True:
-        model.add_random_tile()
-        pyramid(10, model.state._state)
-        scene.waitfor("keyup")
-except ImpossibleToFinishException:
-    pass
+    o = vec(0, 0, 0)
+    arrow(pos=o, axis=vec(10, 0, 0), color=color.red, opacity=OPACITY)
+    arrow(pos=o, axis=vec(0, 10, 0), color=color.green, opacity=OPACITY)
+    arrow(pos=o, axis=vec(0, 0, 10), color=color.blue, opacity=OPACITY)
+
+
+def show_pyramid_axes():
+    OPACITY = 0.5
+
+    o = vec(0, 0, 0)
+    arrow(pos=o, axis=vec(10, 0, 0), color=color.red, opacity=OPACITY)
+    arrow(pos=o, axis=vec(5, 5 * sqrt(3), 0), color=color.green,
+          opacity=OPACITY)
+    arrow(pos=o, axis=vec(5, 5/3 * sqrt(3), 5 * sqrt(8/3)), color=color.blue,
+          opacity=OPACITY)
+
+
+def show_model(model):
+    clear()
+    pyramid(model.size, model.state._state)
+
+
+def real_coords(pos):
+    x, y, z = pos
+
+    rz = sqrt(8/3) * RADIUS * z
+
+    ry_start = (sqrt(3) / 3) * RADIUS * z  # Layer offset
+    ry = ry_start + sqrt(3) * RADIUS * y
+
+    rx_start = RADIUS * (y + z)
+    rx = rx_start + 2 * x * RADIUS
+
+    return vec(rx, ry, rz)
+
+
+def show_model2(model):
+    clear()
+    for pos in model._all_positions():
+        val = model.state[pos]
+
+        if val is None:
+            continue
+
+        if val not in colors:
+            colors[val] = random_color()
+        color = colors[val]
+
+        rpos = real_coords(pos)
+        spheres.append(sphere(pos=rpos, radius=RADIUS, color=color,
+                       opacity=0.8))
+
+
+show_pyramid_axes()
+
+model = PyramidCoveringModel(15, 4)
+# model = PyramidCoveringModel(4, 4)
+
+while not model.is_filled():
+    model.add_random_tile()
+    show_model2(model)
+    scene.waitfor("keyup")
