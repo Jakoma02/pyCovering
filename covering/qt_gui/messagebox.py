b8a9d29eb77a8e40bfa9e7b7c8dd49dd2de3ebaf
Jakub Komárek <komaja@email.cz>
2020-09-11 14:18:34 +0200

    Add docstrings for messagebox.py

--- covering/qt_gui/messagebox.py
+++ covering/qt_gui/messagebox.py
@@ -1,3 +1,7 @@
+"""
+A single-class module, see the MessageBox class
+"""
+
 from collections import deque
 from datetime import datetime
 
@@ -5,6 +9,12 @@ from PySide2.QtWidgets import QTextEdit
 
 
 class MessageBox(QTextEdit):
+    """
+    A QTextEdit subclass for displaying log messages.
+
+    It is designed to automatically scroll down only if
+    it is already scrolled down
+    """
     # This helped with auto scrolling down: https://stackoverflow.com/questions
     # /14550146/textedit-scroll-down-automatically-only-if-the-scrollbar-is-at-the-bottom
 
@@ -15,9 +25,12 @@ class MessageBox(QTextEdit):
         super().__init__(parent)
 
         self._messages = deque()
-        self.update_text()
+        self._update_text()
 
     def add_message(self, msg):
+        """
+        Prints a new message in the message area
+        """
         if len(self._messages) == self.MAX_MESSAGES:
             self._messages.popleft()
 
@@ -27,17 +40,23 @@ class MessageBox(QTextEdit):
         complete_msg = f"[{time_str}] {msg}"
 
         self._messages.append(complete_msg)
-        self.update_text()
+        self._update_text()
 
     def is_scrolled_down(self):
+        """
+        Returns True if the scrollbar is at the lowest possible position
+        """
         scrollbar = self.verticalScrollBar()
         return scrollbar.value() == scrollbar.maximum()
 
     def scroll_down(self):
+        """
+        Move the scrollbar to lowest possible position
+        """
         scrollbar = self.verticalScrollBar()
         scrollbar.setValue(scrollbar.maximum())
 
-    def update_text(self):
+    def _update_text(self):
         was_scrolled_down = self.is_scrolled_down()
         scrollbar = self.verticalScrollBar()
         old_scroll_value = scrollbar.value()
