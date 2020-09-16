dcde64526e9a24a34a382109c4d89ac4a8546a78
Jakub Komárek <komaja@email.cz>
2020-09-16 17:39:10 +0200

    Remove data

---
+++ scripts/generate_docs.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# This script is supposed to be run from the project root
+
+epydoc --html -o docs --name pyCovering \
+	--url https://github.com/jakoma02/pyCovering pycovering
